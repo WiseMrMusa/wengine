66db43a23a916a107b7efa7dcfcf67cad17047be
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2025-05-17 16:12:43 +0100

    added server initialization

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,8 @@ import * as fs from "node:fs";
 import * as path from "node:path";
 import { env } from "../shared/utils/env";
 import { logger } from "../shared/utils/logger";
+import fastify, {type FastifyInstance} from "fastify";
+import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 
 // The server connection timeout in milliseconds
 const SERVER_CONNECTION_TIMEOUT = 60_000;
@@ -34,7 +36,52 @@ export const initServer = async () => {
     logger({
         service: "server",
         level: "info",
-        message: "Trust proxy enabled",
+        message: "Server is enabled with trustProxy",
     })
-   }
+    const server: FastifyInstance = fastify({
+        maxParamLength: 200,
+        connectionTimeout: SERVER_CONNECTION_TIMEOUT,
+        disableRequestLogging: true,
+        trustProxy,
+        ...(env.ENABLE_HTTPS ? httpsObject : {}),
+    }).withTypeProvider<TypeBoxTypeProvider>();
+
+    // Configure middleware
+
+    // Register routes
+
+    await server.ready();
+
+   server.listen(
+    {
+        host: env.HOST,
+        port: env.PORT,
+    }, 
+    (err) => {
+        if (err) {
+            logger({
+                service: "server",
+                level: "fatal",
+                message: "Failed to start server",
+                error: err,
+            })
+            process.exit(1);
+        }
+        logger({
+            service: "server",
+            level: "info",
+            message: `Server listening on http://${env.HOST}:${env.PORT}`,
+        })
+    }
+   ) 
+
+   const url = `${env.ENABLE_HTTPS ? "https" : "http"}://${env.HOST}:${env.PORT}`;
+   logger({
+    service: "server",
+    level: "info",
+    message: `Server started at ${url}`,
+   })
+    }
 }
+
+
