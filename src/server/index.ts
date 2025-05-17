ff7e995f146ba2e0d46ba95f397482d64dd7dd72
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2025-05-17 15:50:18 +0100

    added logger

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,8 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { env } from "../shared/utils/env";
+import { logger } from "../shared/utils/logger";
+
 // The server connection timeout in milliseconds
 const SERVER_CONNECTION_TIMEOUT = 60_000;
 
@@ -12,5 +17,24 @@ interface HttpsObject {
 }
 
 export const initServer = async () => {
-    
+    // Enable the server to run on https://localhost:PORT, if ENABLE_HTTPS is provided
+   let httpsObject: HttpsObject | undefined = undefined;
+   if (env.ENABLE_HTTPS){
+    httpsObject = {
+        https: {
+            key: fs.readFileSync(path.join(__dirname, "../certs/key.pem")),
+            cert: fs.readFileSync(path.join(__dirname, "../certs/cert.pem")),
+            passphrase: env.HTTPS_PASSPHRASE,
+        }
+    }
+   } 
+
+   const trustProxy = env.TRUST_PROXY || !!env.ENGINE_TIER;
+   if (trustProxy) {
+    logger({
+        service: "server",
+        level: "info",
+        message: "Trust proxy enabled",
+    })
+   }
 }
---
+++ src/shared/utils/logger.ts
@@ -0,0 +1,110 @@
+import { createLogger, format, transports } from "winston";
+import { env } from "./env";
+
+type LogLevels = typeof env.LOG_LEVEL;
+
+const customLevels: {
+    levels: { [key in LogLevels]: number },
+    colors: { [key in LogLevels]: string }
+} = {
+    levels: {
+        fatal: 0,
+        error: 1,
+        warn: 2,
+        info: 3,
+        debug: 4,
+        trace: 5,
+    },
+    colors: {
+        fatal: "red",
+        error: "red",
+        warn: "yellow",
+        info: "green",
+        debug: "blue",
+        trace: "gray",
+    },
+};
+
+const filterNonErrors = format((info) => {
+    if (info.level === "error" || info.level === "fatal") {
+        return false;
+    }
+    return info;
+});
+
+const filterErrorsAndFatal = format((info) => {
+    if (info.level === "error" || info.level === "fatal") {
+        return info;
+    }
+    return false;
+});
+    
+const colorizeFormat = () => {
+    if (env.NODE_ENV === "development") {
+        return format.colorize({
+            colors: customLevels.colors,
+        });
+    }
+    return format.uncolorize();
+}
+
+const winstonLogger = createLogger({
+    levels: customLevels.levels,
+    level: env.LOG_LEVEL,
+    format: format.combine(
+        format.timestamp(),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json(),
+        colorizeFormat(),
+        format.printf(({level, message, timestamp, error}) => {
+            return `[${timestamp}] ${level}: ${message} - ${error ? `\n${(error as Error).stack}` : ""}`;
+        })
+    ),
+    transports: [
+        new transports.Console({
+            format: format.combine(filterNonErrors()),
+        }),
+        new transports.Console({
+            format: format.combine(filterErrorsAndFatal()),
+            stderrLevels: ["error", "fatal"],
+        }),
+    ],
+});
+
+interface LoggerParams {
+    service: (typeof env)["LOG_SERVICES"][0]
+    level: (typeof env)["LOG_LEVEL"]
+    message: string
+    queueId?: string
+    error?: unknown;
+    data?: unknown;
+}
+
+export const logger = ({
+    service,
+    level,
+    message,
+    queueId,
+    error,
+    data,
+}: LoggerParams) => {
+    if (!env.LOG_SERVICES.includes(service)) {
+        return;
+    }
+    let prefix = `[${service.charAt(0).toUpperCase() + service.slice(1)}]`;
+    if (queueId) {
+        prefix += `[Transaction] [${queueId}]`;
+    }
+
+    let suffix = "";
+    if (data){
+        suffix += ` - ${JSON.stringify(data)}`;
+    }
+    
+    if (error){
+        winstonLogger.error(level, `${prefix} ${message}${suffix}`, {error});
+    } else {
+        winstonLogger.log(level, `${prefix} ${message}${suffix}`);
+    }
+}
