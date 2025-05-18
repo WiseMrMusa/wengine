88bdb4acde6ba43eb139cf6fe7e1f2fe1fb5a050
Musa AbdulKareem <abdulkareemusa01@gmail.com>
2025-05-18 16:35:06 +0100

    feat(permissions): implement deleting wallet permission

---
+++ src/shared/db/permissions/delete-permissions.ts
@@ -0,0 +1,15 @@
+import { prisma } from "../client.js";
+
+interface DeletePermissionsParams {
+    walletAddress: string;
+}
+
+export const deletePermissions = async ({
+    walletAddress
+}: DeletePermissionsParams) => {
+    return prisma.permissions.delete({
+        where: {
+            walletAddress: walletAddress.toLowerCase(),
+        }
+    });
+}
